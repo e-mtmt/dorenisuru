3a0d664e7637bafbcfca771790d9b8f317d0a269
ermtmt <domatsu@sonicgarden.jp>
2017-05-11 14:25:52 +0900

    initial

---
+++ src/main/java/jp/eunika/DorenisuruApplication.java
@@ -0,0 +1,11 @@
+package jp.eunika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorenisuruApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DorenisuruApplication.class, args);
+	}
+}
---
+++ src/main/java/jp/eunika/web/TestController.java
@@ -0,0 +1,20 @@
+package jp.eunika.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
+
+	@GetMapping
+	public Integer test() {
+		return jdbcTemplate.queryForObject("select 1 + 1", new MapSqlParameterSource(), Integer.class);
+	}
+}
