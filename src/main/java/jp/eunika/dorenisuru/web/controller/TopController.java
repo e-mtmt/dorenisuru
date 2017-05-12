bd04f4ced580df04359796d033fe5d232f7b53f1
ermtmt <domatsu@sonicgarden.jp>
2017-05-12 18:51:35 +0900

    画面系のリソースを整備

---
+++ src/main/java/jp/eunika/dorenisuru/web/controller/TopController.java
@@ -0,0 +1,14 @@
+package jp.eunika.dorenisuru.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TopController {
+	@GetMapping
+	public String index() {
+		return "index";
+	}
+}
