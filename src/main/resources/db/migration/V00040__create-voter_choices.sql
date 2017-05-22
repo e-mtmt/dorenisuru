ec0e584c636a487b1d89e427ae6ee141cfc9c30c
ermtmt <domatsu@sonicgarden.jp>
2017-05-22 15:52:50 +0900

    DBマイグレーションをリファクタ

--- src/main/resources/db/migration/V00040__create-voter_choices.sql
+++ src/main/resources/db/migration/V00040__create-voter_choices.sql
@@ -1,9 +1,10 @@
 create table voter_choices(
   id         serial      primary key,
   feeling    varchar(10) not null default 'Unknown',
-  comment    varchar(50) not null,
   voter_id   integer     not null references voters(id),
   choice_id  integer     not null references choices(id),
   created_at timestamp   not null,
   updated_at timestamp   not null
 );
+
+create unique index idx_voter_choices_on_voter_id_and_choice_id on voter_choices(voter_id, choice_id);
--- src/main/resources/db/migration/V00050__move-columns-voter_choices-to-voters.sql
+++
@@ -1,2 +0,0 @@
-alter table voter_choices drop column comment;
-alter table voters add column comment varchar(50);
