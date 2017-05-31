15519a0d100c5ee91f7e2ce015c72e5bfccf7f76
enk-mtmt <matsumoto@eunika.jp>
2017-05-31 11:04:37 +0900

    [#15] CSRF対策を設定

--- src/main/java/jp/eunika/dorenisuru/web/controller/ApplicationController.java
+++ src/main/java/jp/eunika/dorenisuru/web/controller/ApplicationController.java
@@ -4,17 +4,20 @@ import javax.persistence.EntityNotFoundException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 public abstract class ApplicationController {
-	@ExceptionHandler(SecurityException.class)
+	@RequestMapping(value = "/403")
 	@ResponseStatus(HttpStatus.FORBIDDEN)
+	@ExceptionHandler(SecurityException.class)
 	public String handleSecurityException() {
 		return "error-403";
 	}
 
-	@ExceptionHandler(EntityNotFoundException.class)
+	@RequestMapping(value = "/404")
 	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(EntityNotFoundException.class)
 	public String handleEntityNotFoundException() {
 		return "error-404";
 	}
